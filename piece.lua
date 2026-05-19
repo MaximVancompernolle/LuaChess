f3bd63acc35d2d3f3c4b22c5bc07ba46ffa4943e
PacManMVC <maxim.vancompernolle@gmail.com>
2026-05-19 15:38:56 -0400

    WIP refactor: create new bitwise Piece class

--- piece.lua
+++ piece.lua
@@ -1,70 +1,47 @@
----@class Piece
+local ffi = require('ffi')
+local bit = require('bit')
+local bnot, band, bor, bxor = bit.bnot, bit.band, bit.bor, bit.bxor
+local lshift, rshift = bit.lshift, bit.rshift
+
+---@Class Piece
 Piece = Object:extend()
 
---[[
-	TODO represent piece as binary object
-	none = 0
-	king = 1
-	pawn = 2
-	knight = 3
-	bishop = 5
-	rook = 6
-	queen = 7
+NONE = 0
+KING = 1
+PAWN = 2
+KNIGHT = 3
+BISHOP = 5
+ROOK = 6
+QUEEN = 7
 
-	white = 8
-	black = 16
+WHITE = 8
+BLACK = 16
 
-	use bitwise operands to get color/type of piece
-	typeMask = 0b00111
-	whiteMask = 0b01000
-	blackMask = 0b10000
-	colorMask = whiteMask | blackMask
-]]
+typeMask = ffi.new('uint8_t', 7)
+whiteMask = ffi.new('uint8_t', 8)
+blackMask = ffi.new('uint8_t', 16)
+colorMask = bor(whiteMask, blackMask)
 
-function Piece:init(char)
-	local b = string.byte(char)
-
-	-- WHITE = 1
-	-- black = -1
-	if b >= 65 and b <= 90 then
-		self.color = 1
-	elseif b >= 97 and b <= 122 then
-		self.color = -1
-	end
-
-	self.type = char
-	
-	-- used by pawn for double pushes and king for castling
-	-- TODO remove this field, it's not necessary
-	self.hasMoved = false
+function Piece.isColor(piece, color)
+	return band(piece, colorMask) == color
 end
 
-function Piece:isSlidingPiece()
-	local char = self.type:lower()
-	
-	return char == 'b' or char == 'r' or char == 'q'
+function Piece.color(piece)
+	return band(piece, colorMask)
 end
 
-function Piece:isQueen()
-	return self.type:lower() == 'q'
+function Piece.type(piece)
+	return band(piece, typeMask)
 end
 
-function Piece:isBishop()
-	return self.type:lower() == 'b'
+function Piece.isRookOrQueen(piece)
+	return band(piece, ffi.new('uint8_t', 6)) == 6
 end
 
-function Piece:isRook()
-	return self.type:lower() == 'r'
+function Piece.isBishopOrQueen(piece)
+	return band(piece, ffi.new('uint8_t', 5)) == 5
 end
 
-function Piece:isKnight()
-	return self.type:lower() == 'n'
-end
-
-function Piece:isPawn()
-	return self.type:lower() == 'p'
-end
-
-function Piece:isKing()
-	return self.type:lower() == 'k'
+function Piece.isSlidingPiece(piece)
+	return band(piece, ffi.new('uint8_t', 4)) ~= 0
 end
