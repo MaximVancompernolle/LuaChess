d788dadefcabd617bfaadc70d9e84f0b5a1bd4ab
PacManMVC <maxim.vancompernolle@gmail.com>
2026-05-21 15:10:30 -0400

    WIP refactor: PCMD now stored in global variables

--- precomputed_move_data.lua
+++ precomputed_move_data.lua
@@ -1,4 +1,24 @@
-function MoveGenerator:precomputedMoveData()
+local ffi = require('ffi')
+local bit = require('bit')
+local bnot, band, bor, bxor = bit.bnot, bit.band, bit.bor, bit.bxor
+local lshift, rshift = bit.lshift, bit.rshift
+
+slidingOffsets = {8, -8, -1, 1, 7, -7, 9, -9}
+numSquaresToEdge = {}
+kingMoves = {}
+kingAttackBitBoards = {}
+pawnAttackBitBoards = {
+	[1] = {},
+	[-1] = {},
+}
+knightMoves = {}
+knightAttackBitBoards = {}
+rookMoves = {}
+bishopMoves = {}
+queenMoves = {}
+rayLookup = {}
+
+function precomputeMoveData()
 	local knightOffsets = {6, -6, 10, -10, 15, -15, 17, -17}
 	local mask = ffi.new('uint64_t', 1)
 
@@ -11,7 +31,7 @@ function MoveGenerator:precomputedMoveData()
 
 			local index = file + ((rank - 1) * 8)
 
-			self.numSquaresToEdge[index] = {
+			numSquaresToEdge[index] = {
 				numNorth,
 				numSouth,
 				numWest,
@@ -22,37 +42,21 @@ function MoveGenerator:precomputedMoveData()
 				math.min(numSouth, numWest),
 			}
 
-			self.knightMoves[index] = {}
-			local knightBitBoard = ffi.new('uint64_t', 0)
-			for offsetIndex = 1, #knightOffsets do
-				local knightEndSquare = index + knightOffsets[offsetIndex]
-				if knightEndSquare >= 1 and knightEndSquare <= 64 then
-					local knightSquareX = ((knightEndSquare - 1) % 8) + 1
-					local knightSquareY = math.floor((knightEndSquare - 1) / 8) + 1
-
-					if math.max(math.abs(file - knightSquareX), math.abs(rank - knightSquareY)) == 2 then
-						table.insert(self.knightMoves[index], knightEndSquare)
-						knightBitBoard = bor(knightBitBoard, lshift(mask, knightEndSquare - 1))
-					end
-				end
-			end
-			self.knightAttackBitBoards[index] = knightBitBoard
-
-			self.kingMoves[index] = {}
+			kingMoves[index] = {}
 			local kingBitBoard = ffi.new('uint64_t', 0)
-			for offsetIndex = 1, #self.slidingOffsets do
-				local kingEndSquare = index + self.slidingOffsets[offsetIndex]
+			for offsetIndex = 1, #slidingOffsets do
+				local kingEndSquare = index + slidingOffsets[offsetIndex]
 				if kingEndSquare >= 1 and kingEndSquare <= 64 then
 					local kingSquareX = ((kingEndSquare - 1) % 8) + 1
 					local kingSquareY = math.floor((kingEndSquare - 1) / 8) + 1
 
 					if math.max(math.abs(file - kingSquareX), math.abs(rank - kingSquareY)) == 1 then
-						table.insert(self.kingMoves[index], kingEndSquare)
+						table.insert(kingMoves[index], kingEndSquare)
 						kingBitBoard = bor(kingBitBoard, lshift(mask, kingEndSquare - 1))
 					end
 				end
 			end
-			self.kingAttackBitBoards[index] = kingBitBoard
+			kingAttackBitBoards[index] = kingBitBoard
 
 			local pawnBitBoardWhite = ffi.new('uint64_t', 0)
 			local pawnBitBoardBlack = ffi.new('uint64_t', 0)
@@ -72,28 +76,44 @@ function MoveGenerator:precomputedMoveData()
 					pawnBitBoardBlack = bor(pawnBitBoardBlack, lshift(mask, index - 8))
 				end
 			end
-			self.pawnAttackBitBoards[1][index] = pawnBitBoardWhite
-			self.pawnAttackBitBoards[-1][index] = pawnBitBoardBlack
+			pawnAttackBitBoards[1][index] = pawnBitBoardWhite
+			pawnAttackBitBoards[-1][index] = pawnBitBoardBlack
+
+			knightMoves[index] = {}
+			local knightBitBoard = ffi.new('uint64_t', 0)
+			for offsetIndex = 1, #knightOffsets do
+				local knightEndSquare = index + knightOffsets[offsetIndex]
+				if knightEndSquare >= 1 and knightEndSquare <= 64 then
+					local knightSquareX = ((knightEndSquare - 1) % 8) + 1
+					local knightSquareY = math.floor((knightEndSquare - 1) / 8) + 1
+
+					if math.max(math.abs(file - knightSquareX), math.abs(rank - knightSquareY)) == 2 then
+						table.insert(knightMoves[index], knightEndSquare)
+						knightBitBoard = bor(knightBitBoard, lshift(mask, knightEndSquare - 1))
+					end
+				end
+			end
+			knightAttackBitBoards[index] = knightBitBoard
 
 			local rookBitBoard = ffi.new('uint64_t', 0)
 			for directionIndex = 1, 4 do
-				for n = 1, self.numSquaresToEdge[index][directionIndex] do
-					local rookEndSquare = index + self.slidingOffsets[directionIndex] * n
+				for n = 1, numSquaresToEdge[index][directionIndex] do
+					local rookEndSquare = index + slidingOffsets[directionIndex] * n
 					rookBitBoard = bor(rookBitBoard, lshift(mask, rookEndSquare - 1))
 				end
 			end
-			self.rookMoves[index] = rookBitBoard
+			rookMoves[index] = rookBitBoard
 
 			local bishopBitBoard = ffi.new('uint64_t', 0)
 			for directionIndex = 5, 8 do
-				for n = 1, self.numSquaresToEdge[index][directionIndex] do
-					local bishopEndSquare = index + self.slidingOffsets[directionIndex] * n
+				for n = 1, numSquaresToEdge[index][directionIndex] do
+					local bishopEndSquare = index + slidingOffsets[directionIndex] * n
 					bishopBitBoard = bor(bishopBitBoard, lshift(mask, bishopEndSquare - 1))
 				end
 			end
-			self.bishopMoves[index] = bishopBitBoard
+			bishopMoves[index] = bishopBitBoard
 
-			self.queenMoves[index] = bor(rookBitBoard, bishopBitBoard)
+			queenMoves[index] = bor(rookBitBoard, bishopBitBoard)
 		end
 	end
 
@@ -110,6 +130,6 @@ function MoveGenerator:precomputedMoveData()
 			absDir = 7
 		end
 
-		self.rayLookup[i] = absDir * sign(offset)
+		rayLookup[i] = absDir * sign(offset)
 	end
 end
